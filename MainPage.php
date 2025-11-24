a63b8b8f4ef1ea77ad50ff9ad8d16c7cd260538d
アビ (Tippy) <gamerabhi124@gmail.com>
2025-11-25 03:17:03 +0700

    First Commit

---
+++ MainPage.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hari Guru</title>
+</head>
+<body>
+    <center>
+    <h1>Selamat Hari Guru</h1>
+
+    </center>
+</body>
+</html>
